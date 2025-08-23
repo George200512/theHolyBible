bb2c3d3a1acb7fe4cf212d3060d15931d153ed77
Alpha <georgeofori2005@gmail.com>
2025-08-23 10:58:17 +0000

    22nd August, 2025(2)

--- verses/test.py
+++ verses/test.py
@@ -1,30 +1,132 @@
+# Author: Ofori George
+# Commencement Date: Thursday 21 August, 2025.
+# Telephone / WhatsApp: 0504694485
+# Facebook: Street Python
+# Email: georgeofori2005@gmail.com
+# website: not ready yet
+# github: https://www.github.com/George200512/
+
+
 import unittest
-import sqlite3 
+import sqlite3
 import os
 
-from verse import Verse
+from verse import Verse, VerseArray
 
-path = "../test.db"
+PATH = "../test.db"
 
-conn = sqlite3.connect(path)
+conn = sqlite3.connect(PATH)
+
 
 class TestVerse(unittest.TestCase):
     """Test the methods of the verse object"""
-     
+
+    @classmethod
+    def tearDownClass(cls):
+        """Close database and delete file"""
+
+        conn.close()
+        os.remove(PATH)
+
     def test_get_id(self):
         """Test the validity of the get_id method of the verse class"""
-        
-        verse = Verse(conn, text="And Jesus wept.", book="John", verse_no=31, chapter_no=10) 
+
+        verse = Verse(
+            conn, text="And Jesus wept.", book="John", verse_no=31, chapter_no=10
+        )
         self.assertEqual(verse.id, 2)
-        
+
     def test_add_verse_if_not_added(self):
         """Checks if a verse is added to it appropriately"""
-        
-        verse = Verse(conn, text="The lord shall fight my fight", book="Exodus", verse_no=14, chapter_no=14)
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
         self.assertEqual(str(verse), "Verse 14")
-        
-        
+
+    def test_create_verse_table_if_not_exists(self):
+        """Check if table if created correctly"""
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
+        self.assertEqual(isinstance(verse, Verse), True)
+
+    def test_value(self):
+        """Test if my value property return the text of the verse"""
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
+        self.assertEqual(verse.value, "The lord shall fight my fight")
+
+    def test_chapter(self):
+        """Test if the chapter property returns the right chapter number"""
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
+        self.assertEqual(verse.chapter, 14)
+
+    def test_book(self):
+        """Test if the book property returns the right book name"""
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
+        self.assertEqual(verse.book, "Exodus")
+
+    def test_verse(self):
+        """Test if the verse property returns the right verse number"""
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
+        self.assertEqual(verse.verse, 14)
+
+    def test_id(self):
+        """Test if the id property returns the right id"""
+
+        verse = Verse(
+            conn,
+            text="The lord shall fight my fight",
+            book="Exodus",
+            verse_no=14,
+            chapter_no=14,
+        )
+        self.assertEqual(verse.id, 1)
+
+
+# A VerseArray testCase class
+class TestVerseArray(unittest.TestCase):
+    """A unittest testcase class for the verse array class"""
+    
+    pass
+    
+    
 if __name__ == "__main__":
     unittest.main()
-    os.remove(path)
-    
--- verses/verse.py
+++ verses/verse.py
@@ -1,7 +1,7 @@
-# Author: Ofori George 
+# Author: Ofori George
 # Commencement Date: Thursday 21 August, 2025.
 # Telephone / WhatsApp: 0504694485
-# Facebook: Street Python 
+# Facebook: Street Python
 # Email: georgeofori2005@gmail.com
 # website: not ready yet
 # github: https://www.github.com/George200512/
@@ -17,7 +17,7 @@ from collections import UserString, UserList
 # Create a verse class to represent a single a verse.
 class Verse(UserString):
     """Initialise the arguments passed to the constructor"""
-    
+
     def __init__(self, connection, text, chapter_no, book, verse_no):
         """
         connection: a connection object for creating the cursor which will be used to
@@ -27,35 +27,36 @@ class Verse(UserString):
         book: a string representing the book.
         verse_no: a string representing the verse number.
         """
-        
+
         self.conn = connection
-        self.cursor = self.conn.cursor() #The cursor object for querying the database. 
-        self.text = text
-        self.chapter = chapter_no
-        self.book = book
-        self.verse = verse_no 
+        self.cursor = self.conn.cursor()  # The cursor object for querying the database.
+        self._text = text
+        self._chapter = chapter_no
+        self._book = book
+        self._verse = verse_no
         self.create_verse_table_if_not_exists()
-        self.id = self.get_id()
+        self._id = self.get_id()
         self.add_verse_if_not_added()
-        super().__init__(self.text)
-        
+        super().__init__(self._text)
+
     def get_id(self):
         """Generate the id of the verse if it has none or return the existing one"""
-        
+
         self.cursor.execute(
             """
             SELECT id FROM verses WHERE text=? AND chapter_no=? AND  verse_no=? AND book=?;
-            """, (self.text, self.chapter, self.verse, self.book)
+            """,
+            (self._text, self._chapter, self._verse, self._book),
         )
         row = self.cursor.fetchone()
         if row:
             return row[0]
         id = self.add_verse_if_not_added()[0]
         return id
-        
+
     def create_verse_table_if_not_exists(self):
         """Create the verse table in the database if it is not present"""
-        
+
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS verses (
@@ -69,40 +70,100 @@ class Verse(UserString):
             """
         )
         self.conn.commit()
-       
+
     def add_verse_if_not_added(self):
-       """Add a new verse to the database if it has not been added."""
-       
-       self.cursor.execute(
+        """Add a new verse to the database if it has not been added."""
+
+        self.cursor.execute(
             """
             SELECT id FROM verses WHERE text=? AND chapter_no=? AND verse_no=?;
             """,
-            (self.text, self.chapter, self.verse)
+            (self._text, self._chapter, self._verse),
         )
-       row = self.cursor.fetchone()
-       if row:
-           return row
-       self.cursor.execute(
-        """
+        row = self.cursor.fetchone()
+        if row:
+            return row
+        self.cursor.execute(
+            """
         INSERT INTO verses(text, chapter_no, verse_no,  book) VALUES (?, ?, ?, ?);
-        """, (self.text, self.chapter, self.verse, self.book)
+        """,
+            (self._text, self._chapter, self._verse, self._book),
         )
-       self.conn.commit()
-       self.cursor.execute(
+        self.conn.commit()
+        self.cursor.execute(
             """
             SELECT id FROM verses WHERE text=? AND chapter_no=? AND verse_no=?;
             """,
-            (self.text, self.chapter, self.verse)
+            (self._text, self._chapter, self._verse),
         )
-       row = self.cursor.fetchone()
-       return row
-        
+        row = self.cursor.fetchone()
+        return row
+
     def __repr__(self):
         """Return a representation of the verse that can be used to create a verse"""
-        
+
         return f"Verse(connection={self.conn}, text={self.text}, chapter_no={self.chapter},  book={self.book}, verse_no={self.verse})"
-        
+
     def __str__(self):
-        """A string representation of a verse """
-        
+        """A string representation of a verse"""
+
         return f"Verse {self.verse}"
+
+    @property
+    def value(self):
+        """Show the actual text"""
+
+        return self._text
+
+    @property
+    def chapter(self):
+        """Return the chapter number"""
+
+        return self._chapter
+
+    @property
+    def book(self):
+        """Return the book name"""
+
+        return self._book
+
+    @property
+    def verse(self):
+        """Return the verse number"""
+
+        return self._verse
+
+    @property
+    def id(self):
+        """return the id of a verse"""
+
+        return self._id
+
+
+# Create a VerseArray class to represent a collection of related or not related verses
+class VerseArray(UserList):
+    """A class representing a group of verses"""
+
+    def __init__(self, array=[]):
+        """Initialise the variables needed feo creating the class"""
+
+        self.array = array
+        if isinstance(self.array, (list, tuple)):
+            super().__init__(self.array)
+        raise ValueError("Array must be a tuple or a list")
+
+    def __iter__(self):
+        """A dunder method when the array is being iterated over"""
+
+        for verse in self.array:
+            yield verse
+
+    def __str__(self):
+        """A string representation of the class"""
+
+        return f"[<VerseArray:{len(self.array)}>]"
+
+    def __repr__(self):
+        """A string representing how an object of the class should be created"""
+
+        return f"VerseArray(array={self.array})"
