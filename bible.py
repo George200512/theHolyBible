27478a062022d2a6c2932f0467b106a4ecb49eb0
Alpha <georgeofori2005@gmail.com>
2025-10-12 13:58:57 +0000

    Oct 12, 2025

--- bible.py
+++ bible.py
@@ -11,6 +11,7 @@ A script to create the bible class and the compiler class
 """
 from collections import UserList
 import sqlite3
+import json
 
 from books.book import BookArray, Book
 from books import exceptions as exc
@@ -28,11 +29,12 @@ class Bible(UserList):
         self.name = name
         if self. name is None:
             self. path = utils.get_settings()["DATABASES"].get("DEFAULT")
-        self.path = utils. get_settings()["DATABASES"].get(name)
+        else:
+            self.path = utils. get_settings()["DATABASES"].get(name)
         if self. path is None:
             raise bexc. BibleNotAvailableError("Requested bible not available in database")
         self. connection = sqlite3. connect(self.path)
-        self.nof_of_books = 66
+        self.no_of_books = 66
         self. book_array = BookArray([Book(self.connection, num) for num in range(1, self.no_of_books + 1)])
         super().__int__(self. book_array)
         
@@ -52,9 +54,9 @@ class Bible(UserList):
     def __str__(self):
         """A string representation of the bible class"""
         
-        version = utils. version(self.name)
-        language = utils.language(self. name)
-        return f"{version}({language})"
+        self.version = utils. version(self.name)
+        self.language = utils.language(self.name)
+        return f"{self.version}({self.language})"
         
     def __repr__(self):
         """A string representation of how the class should be instantiated"""
@@ -96,7 +98,7 @@ class Bible(UserList):
                 step = 1
             return self.book_array[start - 1 : stop : step]
         else:
-            return self.data[index]
+            return self.book_array[index]
             
     def book(self, num):
         """Get the book of the bible based on the number
@@ -109,3 +111,99 @@ class Bible(UserList):
         except exc.BookNotFoundError:
             return None
             
+    def head(self, quantity=5):
+        """Get the first five books of a bible if it is more than five or all books
+        quantity:the number of books to return.Default is 5.
+        Returns:BookArray
+        """
+
+        return self.book_array[:quantity]
+
+    def tail(self, quantity=5):
+        """Get the last five book of a bible if it is more than five or all books
+        quantity:the number of books to return.Default is 5.
+        Returns: BookArray 
+        """
+        
+        return self.book_array[-quantity:]
+        
+    def book(self, number=1):
+        """Get the book of the bible by providing its number
+        number:an integer representing the book number
+        RETURNS:A book instance if no error occurs or none if it occurs
+        """
+
+        try:
+            return self[number]
+        except exc.BookNotFoundError:
+            return None
+
+    def verses(self, start=1, stop=1, step=None):
+        """Get a list of book from the bible
+        start:where to start slicing the bible defaults to 1
+        stop:where to stop slicing the bible defaults to 1
+        step:how many steps to take before the next slice defaults to none which is equal to 1
+        RETURNS:A book array instance
+        """
+
+        try:
+            return self[start:stop:step]
+        except exc.BookNotFoundError:
+            return BookArray([])
+            
+    def testament(self, type):
+        """
+        Get the books of the same type.
+        type:(string); whether old or new.
+        RETURNS:Book array.
+         """
+          
+        if not isinstance(type, string):
+          raise TypeError("Argument should be of type string.")
+        if type == "old":
+          return self[:39]
+        elif type == "new":
+          return self[40:]
+        else:
+          return None
+          
+    def describe(self):
+          """
+          Returns a json that gives a discription of the bible including version, language, 
+          number of books, chapters, verses etc.   
+          """
+          
+          no_of_chapters = sum(map(lambda book_id : _get_number_of_chapters(book_id), range(1, 66 + 1)))
+          no_of_verses = 0;
+          for book, book_id in enumerate(self, start=1):
+              for chapter, chapter_id in enumerate(book, start=1):
+                  no_of_verses += _get_number_of_verses(book_id, chapter_id)
+                  
+          data = {
+          "name": self.name,
+          "version": self.version,
+          "language": self.language,
+          "number_of_books": self.no_of_books,
+          "number_of_chapters": no_of_chapters,
+          "number_of_verses": no_of_verses, 
+          }
+          return json.dumps(data)
+          
+    def _get_number_of_chapters(self, book_number):
+        """
+        Get the number of chapters in a book.
+        book_number(int):the nth book of the bible. Starts from 1.
+        RETURN: an integer
+        """
+        
+        return len(self[book_number])
+        
+    def _get_number_of_verses(self, book_number, chapter_number):
+        """
+        Get the number of verses in a chapter
+        chapter_number(int): the nth chapter of a book. Starts from 1.
+        book_number(int): the nth book of the bible. Starts from 1.
+        RETURN: an integer.
+        """ 
+        
+        return len(self[book_number][chapter_number])
